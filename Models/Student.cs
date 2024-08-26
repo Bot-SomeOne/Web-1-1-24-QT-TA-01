a2b56e23e80bc8899bb7305808bc6d9e9e082929
V <lytranvinh.work@gmail.com>
2024-08-26 22:21:46 +0700

    [ADD]: Add file Student

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+using lab1.models;
+using lab1.Models;
+
+class Student {
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public Branch? Branch { get; set; }
+    public Gender? Gender { get; set; }
+    public bool IsRegular { get; set; } // Hệ: true - chính qui; false - phi chính qui
+    public string? Address { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+}
