014ccd353cd2eb3b82346d363f006298974fe399
V <lytranvinh.work@gmail.com>
2024-08-27 01:48:27 +0700

    [SAVE]

--- obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b1f537d272d2dc604de2ba21f59e7f74f2561d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509db8e8b5e5a6967cf02252e8eb1def82d0374e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
