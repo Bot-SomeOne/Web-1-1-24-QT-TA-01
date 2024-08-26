75b779db9af7e07d01f57ecb1d9fcfd95a95c3c6
V <lytranvinh.work@gmail.com>
2024-08-26 22:43:27 +0700

    [ADD]: Add file student controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+
+using lab1.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace lab1.Controllers;
+
+class StudentController : Controller {
+    // Variables
+    private List<Student> listStudent = new List<Student>();
+
+    // Constructor
+    public StudentController() {
+        this.listStudent = new List<Student>(){
+            new Student() {
+                Id = 101,
+                Name = "Hai Nam",
+                Branch = Branch.IT,
+                Gender = Gender.Male,
+                IsRegular = true,
+                Address = "A1-2018",
+                Email = "nam@gmail.com"
+            },
+            new Student() {
+                Id = 102,
+                Name = "Minh Tu",
+                Branch = Branch.BE,
+                Gender = Gender.Female,
+                IsRegular = true,
+                Address = "A1-2019",
+                Email = "tu@gmail.com"
+            },
+        };
+    }
+
+    // Actions
+
+    // GET: Student
+    public IActionResult Index() {
+        return View(this.listStudent);
+    }
+}
