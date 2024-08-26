bd76096a5def321f8d66b1e1de88aa31860ab99b
V <lytranvinh.work@gmail.com>
2024-08-26 22:07:41 +0700

    [Upgrade]: create route config to new file

---
+++ Route.cs
@@ -0,0 +1,16 @@
+namespace lab1;
+
+class Route {
+    public static void RouteConfig(WebApplication app) {
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    }
+}
