1a5c5a1a27665cc226b671c271555f7e8278a1f3
V <lytranvinh.work@gmail.com>
2024-08-27 23:43:51 +0700

    [Upgrade]: Add name display

--- Models/Branch.cs
+++ Models/Branch.cs
@@ -1,8 +1,19 @@
-namespace lab1.models;
+using System.ComponentModel.DataAnnotations;
 
-public enum Branch {
-    IT,
-    BE,
-    CE,
-    EE
+namespace lab1.models
+{
+    public enum Branch
+    {
+        [Display(Name = "Công nghệ thông tin")]
+        IT = 1,
+
+        [Display(Name = "Kinh tế")]
+        BE = 2,
+
+        [Display(Name = "Công trình")]
+        CE = 3,
+
+        [Display(Name = "Điện - Điện tử")]
+        EE = 4
+    }
 }
