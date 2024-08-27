a905a498816816f8f910bd04275dcc6ef7592ad8
V <lytranvinh.work@gmail.com>
2024-08-27 23:44:28 +0700

    [Upgrade]: add data type

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace lab1.models;
 
@@ -6,12 +7,18 @@ public class Student
 
     public int Id { get; set; }
     public string? Name { get; set; }
+
+    [DataType(DataType.EmailAddress)]
     public string? Email { get; set; }
+
+    [DataType(DataType.Password)]
     public string? Password { get; set; }
     public Branch? Branch { get; set; }
     public Gender? Gender { get; set; }
     public bool IsRegular { get; set; } // Hệ: true - chính qui; false - phi chính qui
     public string? Address { get; set; }
+
+    [DataType(DataType.Date)]
     public DateTime? DateOfBirth { get; set; }
     // public string? AvatarPath { get; set; }
     public byte[]? Avatar { get; set; }
