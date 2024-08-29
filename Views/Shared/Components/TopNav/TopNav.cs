9c04ddf6293409560c98526bd44494f1d2db13be
V <lytranvinh.work@gmail.com>
2024-08-29 23:22:36 +0700

    [Upgrade]: Use view component change partial

---
+++ Views/Shared/Components/TopNav/TopNav.cs
@@ -0,0 +1,34 @@
+
+using Microsoft.AspNetCore.Mvc;
+using lab1.models;
+
+public class TopNav : ViewComponent
+{
+    // Variables
+    private List<NavItem> listNavItems = new List<NavItem>();
+
+    // Constructor
+    public TopNav() {
+        listNavItems = new List<NavItem>(){
+            new NavItem() {
+                Controller = "Home",
+                Action = "Index",
+                Text = "Home"
+            },
+            new NavItem() {
+                Controller = "Home",
+                Action = "Privacy",
+                Text = "Privacy"
+            },
+            new NavItem() {
+                Controller = "Student",
+                Action = "Index",
+                Text = "List Students"
+            }   
+        };
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync() {
+        return View("Default", listNavItems);
+    }
+}
