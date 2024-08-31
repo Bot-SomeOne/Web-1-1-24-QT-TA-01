3fab763bc8ca52ecac8f65414f86115fd0f2bb35
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-08-31 15:20:10 +0000

    [Upgrade]: Add 2 acton [GET] create and [POST] create

--- Controllers/LearnerController.cs
+++ Controllers/LearnerController.cs
@@ -24,4 +24,53 @@ public class LearnerController : Controller
         var learners = db.Learners.Include(m => m.Major).ToList();
         return View(learners);
     }
+
+    // GET: Create a new Learner
+    public IActionResult Create()
+    {
+        ViewBag.MajorID = GetViewBagMajorID();
+        return View();
+    }
+
+    // POST: Create a new Learner
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(
+        [Bind("FirstMidName, LastName ,MajorID, EnrollmentDate")]
+            Learner learner
+    )
+    {
+        if (ModelState.IsValid)
+        {
+            db.Learners.Add(learner);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        // Else return to the form 
+        ViewBag.MajorID = GetViewBagMajorID();
+        return View();
+    }
+
+    /**
+     * List Help funtion
+     */
+
+    // Help funtion Get ViewBag MajorID 
+    private List<SelectListItem> GetViewBagMajorID() {
+        var majors = new List<SelectListItem>(); 
+
+        foreach (var item in db.Majors)
+        {
+            majors.Add(
+                new SelectListItem
+                {
+                    Text = item.MajorName,
+                    Value = item.MajorID.ToString()
+                }
+            );
+        }
+
+        return majors; 
+    }
+    
 }
