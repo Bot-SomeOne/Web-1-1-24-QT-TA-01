8c8bb7cfcd7623ba237145638d127b765f44d5c8
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-03 19:49:13 +0000

    [Upgrade]: Change view to area cmp

---
+++ Areas/Admin/Views/Shared/Components/Major/Major.cs
@@ -0,0 +1,28 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+// 
+using lab1.models;
+using lab1.data;
+
+namespace lab1.areas.viewcomponents;
+
+public class Major : ViewComponent
+{
+    // Variables
+    private SchoolContext db;
+    List<lab1.models.Major> majors;
+
+    // Constructor
+    public Major(SchoolContext _context)
+    {
+        db = _context;
+        majors = db.Majors.ToList();
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        // return View("RenderMajor", majors);
+        return View("RenderDropMajor", majors);
+    }
+}
