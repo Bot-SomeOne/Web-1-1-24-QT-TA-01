d45e1c127d4e63e39f1daaeb814dd9125533baa6
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-04 16:07:07 +0000

    [Upgrade]: dotner ef add student to db

--- Migrations/20240904160531_AddStudentToDataBase.Designer.cs
+++ Migrations/20240904160531_AddStudentToDataBase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using lab1.data;
 
@@ -11,9 +12,11 @@ using lab1.data;
 namespace lab1.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20240904160531_AddStudentToDataBase")]
+    partial class AddStudentToDataBase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
