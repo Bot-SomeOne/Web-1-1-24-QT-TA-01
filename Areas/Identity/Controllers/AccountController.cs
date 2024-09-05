b3b85e1934df8cdd4b4f9ffc944d8cdfd004fc5f
V <lytranvinh.work@gmail.com>
2024-09-05 14:53:09 +0700

    Loadding area identity ...

---
+++ Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.areas.identity.controllers;
+
+public class AccountController: Controller {
+    // Var
+
+    // Constructor
+
+    // Action
+
+    /**
+     * Get Login
+     */
+    [HttpGet]
+    public IActionResult Login() {
+        return View();
+    }
+
+    /**
+     * Post Login
+     */
+
+
+    /**
+     * Post Logout
+     */
+
+
+    /**
+     * Get Manage
+     */
+
+}   
