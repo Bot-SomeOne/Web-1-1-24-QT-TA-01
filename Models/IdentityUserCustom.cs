89ef1d3cb5bd13d5d7a3a725dd866a4575c091aa
V <lytranvinh.work@gmail.com>
2024-09-05 15:00:59 +0700

    [ADD]: Add file custom userfields basic

---
+++ Models/IdentityUserCustom.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.AspNetCore.Identity;
+
+namespace lab1.models;
+
+public class IndentityUserCustom : IdentityUser
+{
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public byte[]? ProfilePicture { get; set; }
+
+    // TODO: Add more properties
+}
