f867fee1aa2916f0c25c18a3dd422e087a6847ad
V <lytranvinh.work@gmail.com>
2024-09-05 15:43:45 +0700

    [Upgrade]: Rm name table ....

--- Data/IdentityContext.cs
+++ Data/IdentityContext.cs
@@ -10,7 +10,23 @@ public class IdentityContext : IdentityDbContext<IdentityUserCustom>
 {
     public IdentityContext(
         DbContextOptions<IdentityContext> options
-    ) : base(options) { 
-        
+    ) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+        base.OnModelCreating(builder);
+        // Bỏ tiền tố AspNet của các bảng: mặc định
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var tableName = entityType.GetTableName();
+            if (tableName.StartsWith("AspNet"))
+            {
+                entityType.SetTableName(tableName.Substring(6));
+            }
+        }
     }
 }
