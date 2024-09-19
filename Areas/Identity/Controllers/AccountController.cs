6fb38eceb61706346445d2ee8c95d1316b75cabc
Ly Tran Vinh <88392742+Youknow2509@users.noreply.github.com>
2024-09-19 17:39:48 +0000

    [Upgrade]: handle add user to db;

--- Areas/Identity/Controllers/AccountController.cs
+++ Areas/Identity/Controllers/AccountController.cs
@@ -1,15 +1,23 @@
 
 using Microsoft.AspNetCore.Mvc;
+using BCrypt.Net;
+// 
+using lab1.areas.identity.models.viewmodels;
+using lab1.models;
+using lab1.data;
 
 namespace lab1.areas.identity.controllers;
 
 [Area("Identity")]
 public class AccountController: Controller {
     // Var
-
+    //Var
+    private IdentityContext _context;
     // Constructor
-
-    // Action
+    public AccountController(IdentityContext identityContext)
+    {
+        _context = identityContext;
+    }
 
     /**
      * Get Login
@@ -26,9 +34,23 @@ public class AccountController: Controller {
         return View();
     }
 
-    // Test
-    public IActionResult Index() {
-        return View();
+    /**
+     * Post Register
+     */
+    [HttpPost]
+    public async Task<IActionResult> Register(Register model) {
+        if (ModelState.IsValid) {
+            var user = new IdentityUserCustom {
+                UserName = model.Email,
+                Email = model.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password) // Hash Password
+            };
+            
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        return View("Register");
     }
 
     /**
