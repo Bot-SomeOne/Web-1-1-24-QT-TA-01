b27b4ac258be1ec313577d7f2396bba9a4db85f4
V <lytranvinh.work@gmail.com>
2024-09-20 17:11:18 +0700

    [Add]: Add service

---
+++ Helpers/JwtHelper.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace lab1.helpers;
+
+public class JwtHelper
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtHelper(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateJwtToken(string username)
+    {
+        // Get JWT settings from appsettings.json
+        var jwtSettings = _configuration.GetSection("Jwt");
+        var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
+
+        // Create token handler
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        // Define token descriptor (claims, issuer, audience, signing credentials, etc.)
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+            Expires = DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings["ExpireMinutes"])),
+            Issuer = jwtSettings["Issuer"],
+            Audience = jwtSettings["Audience"],
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        // Create token
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        // Return the token as a string
+        return tokenHandler.WriteToken(token);
+    }
+}
+
